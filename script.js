49db2e67773552425ef232cfab664535b59c3b75
arafatah <arafathowlader890@gmail.com>
2024-12-31 15:57:54 +0600

    update guess number

--- script.js
+++ script.js
@@ -1,61 +1,59 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-document.querySelector('.number').textContent = 14;
-document.querySelector('.score').textContent = 41;
-
-document.querySelector('.guess').value = 12;
-console.log(document.querySelector('.guess').value)
-*/
+/**
+ * Function should work ---
+ * Get number from input
+ * Can check with the secret number - it should come from variable.
+ * If input number is bigger than secret number, it will show too high, and less that that then will show too low. If match then wil show correct number!
+ * There will be a score variable also, it will decrease always if the user guess the number wrong.
+ * If number is correct then the background color will be change to green, HighScore will be set to the score and the ? mark will be change to the correct number.
+ * Again -- all will be reset to the previous style and variable value except for the highScore.
+ * User can't play more than the score number. If he reached that place, he will lost the game.
+ */
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
-const displayMessage = function (message) {
+const displayMessage = message => {
   document.querySelector('.message').textContent = message;
 };
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
-
+document.querySelector('.check').addEventListener('click', () => {
+  console.log('Working ');
+  const guess = +document.querySelector('.guess').value;
   if (!guess) {
-    // document.querySelector('.message').textContent = 'No Number';
-    displayMessage('No Number')
+    displayMessage('No number');
   } else if (guess === secretNumber) {
-    displayMessage('🎉 Correct Number!') ;
+    displayMessage('Correct Number');
+    document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.message').style.width = '30rem';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').style.transition = 'all 0.75s ease';
 
     if (score > highScore) {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-  } else if (guess !== secretNumber) {
+  } else {
     if (score > 1) {
-      displayMessage( guess > secretNumber ? 'Too High!' : 'Too low!')
-       
+      displayMessage(guess > secretNumber ? 'Too High!' : 'Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      displayMessage('💥 You lost the game!') ;
-      document.querySelector('.score').textContent = 0;
+      displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = '0';
     }
   }
 });
 
-document.querySelector('.again').addEventListener('click', function () {
+// reset
+document.querySelector('.again').addEventListener('click', () => {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  displayMessage('Start guessing....') ;
+  displayMessage('Start guessing....');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.message').style.width = '15rem';
 });
